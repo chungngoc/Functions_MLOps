ce2df496c002014dea1acaf04c491e84ab8ba9e8
chungngoc258 <tongngocchungbtqb@gmail.com>
2026-01-31 03:03:59 +0000

    add test for cli

--- test_1.py
+++ test_1.py
@@ -1,4 +1,16 @@
 from calcul import sum
+from mylib.bot import scrape
+from wiki_bot import cli
+from click.testing import CliRunner
 
 def test_sum():
     assert sum(2,3) == 5
+
+def test_scrape():
+    assert "Microsoft" in scrape("Microsoft")
+
+def test_wikibot():
+    runner = CliRunner()
+    result = runner.invoke(cli, ['--name', 'Microsoft'])
+    assert result.exit_code == 0
+    assert 'Microsoft' in result.output
